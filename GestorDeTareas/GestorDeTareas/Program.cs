59df9b7202808d2635760b536f2b9b9a823d1288
Ivan Cabello <ivcbj91@gmail.com>
2026-04-06 13:37:00 +0200

    Validación de fechas: lanza excepción si la fecha final es menor

--- GestorDeTareas/GestorDeTareas/Program.cs
+++ GestorDeTareas/GestorDeTareas/Program.cs
@@ -5,9 +5,9 @@ public class Program
     public static void Main(string[] args)
     {
         List<Tarea> listaTareas = new List<Tarea>();
-        Tarea tarea1 = new Tarea("Comprar alimentos", DateTime.Now.AddDays(2));
+        Tarea tarea1 = new Tarea("Comprar alimentos", DateTime.Now.AddDays(3));
 
-        // Agregar tareas a la lista
+
         listaTareas.Add(tarea1);
   
         
--- GestorDeTareas/GestorDeTareas/Tarea.cs
+++ GestorDeTareas/GestorDeTareas/Tarea.cs
@@ -10,22 +10,32 @@ namespace GestorDeTareas
         public string Titulo { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaVencimiento { get; set; }
-        public EstadoTarea Estado { get; set; } 
+        public EstadoTarea Estado { get; set; }
 
-        public Tarea(string titulo,DateTime fechaVencimiento)
+        public Tarea(string titulo, DateTime fechaVencimiento)
         {
+            FechaCreacion = DateTime.Now;
+            if (fechaVencimiento <= FechaCreacion)
+            {
+                throw new ArgumentException("Fecha fuera de rango");
+            }
+
             TareaId = Guid.NewGuid().GetHashCode(); // Genera un ID único para cada tarea
             Titulo = titulo;
-            Estado=EstadoTarea.Pendiente; 
-            FechaCreacion = DateTime.Now;
-            FechaVencimiento = fechaVencimiento;
+            Estado = EstadoTarea.Pendiente;
 
+            FechaVencimiento = fechaVencimiento;
 
         }
 
         public void EditarTarea(string nuevoTitulo, DateTime nuevaFechaVencimiento)
         {
+
             Titulo = nuevoTitulo;
+            if (nuevaFechaVencimiento <= FechaCreacion) {
+                throw new ArgumentException("Fecha fuera de rango");
+            }
+
             FechaVencimiento = nuevaFechaVencimiento;
         }
 
@@ -33,7 +43,7 @@ namespace GestorDeTareas
         {
             Estado = nuevoEstado;
         }
-         public override string ToString()
+        public override string ToString()
         {
             return $"ID: {TareaId}, Título: {Titulo}, Estado: {Estado}, Fecha de Creación: {FechaCreacion}, Fecha de Vencimiento: {FechaVencimiento}";
         }
