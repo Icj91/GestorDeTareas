a27236f61ff169083de2eeae0b8c22ebf8fa30cf
Ivan Cabello <ivcbj91@gmail.com>
2026-04-14 12:56:15 +0200

    Agregar validaciones

--- GestorDeTareas/GestorTareas.Test/UnitTest1.cs
+++ GestorDeTareas/GestorTareas.Test/UnitTest1.cs
@@ -1,35 +1,55 @@
 ﻿using GestorDeTareas;
 using NUnit.Framework;
+using System.Numerics;
 
 namespace GestorTareas.Test
 {
+    [TestFixture]
     public class Tests
     {
+        private MotorDeTareas _motorTareas;
         [SetUp]
         public void Setup()
         {
+            _motorTareas = new MotorDeTareas();
         }
 
         [Test]
-        public void ObtenerTodas_TresTareasAgregadas()
+        public void Obtener_TresTareasAgregadas()
         {
             // ARRANGE
             TareaLocalizada tareaLocalizada1 = new TareaLocalizada("Tarea en el extranjero1", "Noruega");
             TareaLocalizada tareaLocalizada2 = new TareaLocalizada("Tarea en el extranjero2", "Francia");
             TareaLocalizada tareaLocalizada3 = new TareaLocalizada("Tarea en el extranjero3", "Francia");
-            MotorDeTareas motorTareas = new MotorDeTareas();
-            motorTareas.AgregarTarea(tareaLocalizada1);
-            motorTareas.AgregarTarea(tareaLocalizada2);
-            motorTareas.AgregarTarea(tareaLocalizada3);
+
+            _motorTareas.AgregarTarea(tareaLocalizada1);
+            _motorTareas.AgregarTarea(tareaLocalizada2);
+            _motorTareas.AgregarTarea(tareaLocalizada3);
 
             // ACT
-            motorTareas.MostrarTodaInformacion();
+            _motorTareas.MostrarTodaInformacion();
 
             // ASSERT — completa estas líneas:
-            Assert.That(motorTareas.listaTareas.Count, Is.EqualTo(3));
-
-            /* Incluye aquí tu código */
+            Assert.That(_motorTareas.listaTareas.Count, Is.EqualTo(3));
         }
+
+
+
+        [Test]
+        public void CrearTareaLocalizada_SiEsEspaña_LanzaArgumentException()
+        {
+            
+            // Como el error lo lanza el "new", el Assert.Throws debe envolver al constructor, ya que si creo el objeto antes no me deja
+            var ex = Assert.Throws<ArgumentException>(() => new TareaLocalizada("Título", "España"));
+
+            //Verifico que el mensaje sea el del error
+            Assert.That(ex.Message, Is.EqualTo("España no esta permitido"));
+        }
+
+
+
+
+
 
     }
 }
