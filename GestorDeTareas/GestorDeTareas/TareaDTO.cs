7768c9682a21f9dd39b73c7dd3bd22adf98db5ce
Ivan Cabello <ivcbj91@gmail.com>
2026-04-17 12:35:29 +0200

    Añado propiedades especificas al DTO de tareas

--- GestorDeTareas/GestorDeTareas/TareaDTO.cs
+++ GestorDeTareas/GestorDeTareas/TareaDTO.cs
@@ -12,5 +12,15 @@ namespace GestorDeTareas
         public DateTime FechaCreacion { get; set; }
         public EstadoTarea Estado { get; set; }
         public DateTime FechaPresentacion { get; set; }
+
+
+        //propiedad de tarea localizada
+        public string Lugar { get; set; }
+
+        //propiedad de tarea subtarea
+        public List<string> ListaSubTareas { get; set; }
+
+        //propiedad de tarea con plazo
+        private DateTime FechaVencimiento { get; set; }
     }
 }
